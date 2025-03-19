ddce3578efa951bea4d3ab3e5d2e1f61546af4dc
Amund Nettelhorst Letrud <117391210+letrud@users.noreply.github.com>
2025-03-19 19:40:51 +0100

    +

--- ConvertTo-Git.ps1
+++ ConvertTo-Git.ps1
@@ -684,7 +684,7 @@ foreach ($cs in $sortedHistory) {
         # Make the commit
         git commit -m $commitMessage --allow-empty
 
-        $branchHashTracker["$branchName_$changesetId"] = git rev-parse HEAD
+        $branchHashTracker["$branch_$changesetId"] = git rev-parse HEAD
      
 
         pop-location
