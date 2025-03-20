46d84015f5b1051a298216c2eec908e14d5982e7
Amund Nettelhorst Letrud <117391210+letrud@users.noreply.github.com>
2025-03-20 08:01:16 +0100

    +

--- ConvertTo-Git.ps1
+++ ConvertTo-Git.ps1
@@ -533,7 +533,7 @@ foreach ($cs in $sortedHistory) {
 
             # CHECKOUT: Source and Destination is not the same :  Checkout the source file and move it to the target branch
             if ($sourceRelativePath -ne $relativePath) {
-                $backupHead = git rev-parse --short HEAD 
+                $backupHead = git rev-parse HEAD 
                 git checkout $sourcehash -- $sourceRelativePath
                 git mv -f $sourceRelativePath $relativePath
                 # revert the original sourcerelativePath
@@ -651,7 +651,7 @@ foreach ($cs in $sortedHistory) {
         # Make the commit
         git commit -m $commitMessage --allow-empty
 
-        $branchHashTracker["$branch-$changesetId"] = git rev-parse --short HEAD
+        $branchHashTracker["$branch-$changesetId"] = git rev-parse HEAD
         $hash=$branchHashTracker["$branch-$changesetId"]
         Write-Host "[TFS-$changesetId] [$branch] [$hash] Comitted changes" -ForegroundColor Gray
         pop-location
